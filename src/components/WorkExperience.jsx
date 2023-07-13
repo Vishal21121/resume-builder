72346f124b98d0480db1eea8dd4af43c3b024b11
Vishal21121 <vs84077000@gmail.com>
2023-07-13 14:40:18 +0530

    added: initial files and WorkExperience file

---
+++ src/components/WorkExperience.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WorkExperience = () => {
+  return (
+    <div className=''>
+        <p className='text-blue-700 text-3xl'>Work Experience</p>
+    </div>
+  )
+}
+
+export default WorkExperience
